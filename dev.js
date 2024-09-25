6ae67fe863720826230f59477cc8ec183fc67a5f
Your Name <debabratagiri627@gmail.com>
2024-09-25 15:47:35 +0530

    hi

--- dev.js
+++ dev.js
@@ -1,3 +1,11 @@
-function vote(){
-    var name=document.getElementById("name").value
-}
+$(document).ready(function() {
+  $("#submitButton").click(function() {
+    var name = $("#nameInput").val().trim();
+    if (name !== "") {
+      $("#message").text("April Fool, " + name + "!");
+      $("#result").fadeIn();
+    } else {
+      alert("Please enter your name.");
+    }
+    });
+  });
